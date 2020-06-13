53d3fab4374a29e613166cb3f05d638de56d7dd5
mohi22 <mohi_migue2210@yahoo.com.ar>
2020-06-12 21:57:58 -0300

    modifique el index.php 3 prueba

--- Indexs.php
+++ Indexs.php
@@ -4,19 +4,16 @@
 <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="CSS/estilos.css">
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 	<link rel="icon" href="Logo.png" type="img/text/png"/>
-=======
-=======
+
 	<link rel="stylesheet" type="text/css" href="CSS/estilos_popup.css">
 
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
 
-
->>>>>>> Villanueva
 	<link rel="icon" href="Logo.png" type="img/text/Logo.png"/>
->>>>>>> Villanueva
+
+	<link rel="stylesheet" type="text/css" href="CSS/contenedor2.css">
 	
 <title>Compra venta Zapala</title>
 	
@@ -32,33 +29,68 @@
 			<nav class="navegador">
 				<ul>
 					<li><a href="Indexs.php">Inicio</a></li>
-					<li><a href="HTML/galeria.html">Galeria</a></li>
-					<li><a href="comercios.php">Comercios</a></li>
-					<li><a id="btn-abrir-popup" class="btn-abrir-popup" href="#">login</a></li>
-					<li><a href="registrarse.php">Registrate</a></li>
+					<li><a href="#">Galeria</a></li>
+					<li><a href="#">Comercios</a></li>
+					<li><a id="#" class="btn-abrir-popup" href="#">login</a></li>
+					<li><a href="#">Registrate</a></li>
 				</ul>
 			</nav>
 		</div>
 	</header>
 	<span class="linea"></span>
-	<main class="main">
+	<main class="main" style="text-align: justify">
 		<div class="contenedor">
-		   <h1>Bienvenidos a el mercado concentrador de Zapala</h1>
+			<img class="quedatecasa" src="img/quedateencasa.jpg" alt="">
+			<h1 align="center">Sitio Bajo Construcción.</h1>
+			<h2 align="center">Bienvenidos a ZapalaComercial.com.ar</h2>
+			<h3 align="left">A quién va dirigido...</h3>
 		
-			<p>Gracias por confiar en nuestra localidad, esperamos de corazón esta página ayude a mejorar la situación de cada uno de nuestros comerciantes. </p>
-			<p>Esta página fue creada en conjunto con alumnos y profesores de la Universidad Tecnológica Nacional, Regional Neuquén. </p>
+			<p>Está dirigido a todos los Proveedores (Comerciantes, Productores Locales, Feriantes y Ofrecedores de 
+			   Servicios Independientes), como así también a Consumidores de Servicios de la ciudad a formalizarse en
+			   el uso de una nueva tecnología ofrecida por la Municipalidad de Zapala.
+			</p>
 			
-			<h1>SI SOS COMERCIANTE REGISTRATE!!! ES GRATIS. </h1>
-			<p>Si sos comerciante y te interés vender a través de esta página, podes registrarte ahora y comenzar a vender tus productos. </p>
 			
-			<h3>PODES VENDER LO QUE QUIERAS. </h3>
-			<p>No importa cuál sea tu rubro, vendas lo que vendas publícalo, alguien lo está necesitando. </p>
+			<h3 align="left">De qué se trata...</h3>
+			<p> ZapalaComercial.com.ar es una iniciativa de la Municipalidad de Zapala en conjunto con Profesores 
+				y Alumnos de la Universidad Tecnológica Nacional, Regional Neuquén, con el objetivo de 
+				desarrollar un Sitio de Ventas Online que formalice la comercialización de productos y servicios 
+				como así también a sus oferentes. 
+			</p>
+			
+			<h3 align="left">Cómo funciona... </h3>
+			<p>En el ZapalaComercial.com.ar podrás publicar tus servicios y/o productos para ser adquiridos por 
+			   los ciudadanos de Zapala, los cuales elegirán como pagarlos y su forma de entrega, de acuerdo 
+			   a lo que ofrezcas, asegurando así una forma rápida y segura de comerciar.
+			</p>
+			<h3>Beneficios de sumarte como Proveedor en el Sistema...</h3>
+			
+			<div class="contenedor2">
+				<ul>
+					<li>
+						<img src="img/imagen1.jpg" alt="">
+						<p>
+						   Publicidad para potenciar tu negocio obteniendo mayor visibilidad comercial 
+						   TOTALMENTE GRATIS.
+						</p>
+					</li>
+					<li>
+						<img src="img/imagen2.png" alt="">
+						<p>
+							Esto te permitirá mejorar la relacion con tus viejos clientes y atraer a nuevos.
+						</p>
+					</li>
+				</ul>
+			</div>
 
-			<p>Gracias por confiar en nuestra localidad, esperamos de corazon esta pagina ayude a mejorar la situacion de cada uno de nuestros comerciantes.</p>
-			<p>Esta pagina fue creada en conjunto con alumnos y profesores de la Universidad Tecnologica Nacional, Regional Neuquen.</p>
-			<p>Gracias por confiar en nuestra localidad, esperamos de corazon esta pagina ayude a mejorar la situacion de cada uno de nuestros comerciantes.</p>
-			<p>Esta pagina fue creada en conjunto con alumnos y profesores de la Universidad Tecnologica Nacional, Regional Neuquen.</p>
-		</di>
+			<h3 align="left">Por qué el desarrollo de ZapalaComercial.com.ar.</h3>
+			<p>El desarrollo del sistema busca mejorar las políticas de ofrecimiento de Productos y/o Servicios a 
+			   consumidores de la ciudad para así dar respuesta a Negocios y Emprendimientos locales en cuanto a la 
+			   necesidad de poder comercializar dentro del contexto de la Cuarentena Obligatoria.  Uno de los 
+			   problemas generados por la misma es el intercambio de dinero, y por otro, el acercamiento entre 
+			   personas para las entregas.
+			</p>
+		</div>
 	</main>
 	
 
