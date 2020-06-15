3cb989113de4e71673ecb9e57a380dc8f18c71d2
David Villanueva <david1997@live.com.ar>
2020-06-15 19:02:30 -0300

    cambio a mamp

--- form_ubicacion.php
+++ form_ubicacion.php
@@ -1,4 +1,5 @@
 <?php 
+
     session_start();
     echo("nombre: ".$_SESSION['nombre']);
     echo("<br>");
@@ -24,18 +25,39 @@
     echo("<br>");
     echo("id usuario: ".$_SESSION['id']);
     echo("<br>");
+
+    include_once("proveedor/php/conexion.php");
+    $localidades = $base->query("SELECT * 
+                            FROM localidad")->fetchAll(PDO::FETCH_ASSOC);
+    $provincias = $base->query("SELECT * FROM provincia")->fetchAll(PDO::FETCH_ASSOC);
+
+    $hola= "Hola";
 ?>
 
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="img/Logo.png" type="text/png"/>
-    <link rel="stylesheet" href="CSS/style_register.css">
-    
-<title>Registro proveedor</title>
-    
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="img/Logo.png" type="text/png"/>
+        <link rel="stylesheet" href="CSS/style_register.css">
+        
+    <title>Registro proveedor</title>
+    <script type="text/javascript">
+            // funcion para autocompletar el CP
+            function codigoPostal()
+            {   
+                var localidad = document.getElementById("localidad").value;
+                
+                if(localidad!=""){
+                    document.getElementById("cp").style.display = "none";
+                    document.getElementById("label_cp").style.display = "none";
+                }else{
+                    document.getElementById("cp").style.display = "flex";
+                    document.getElementById("label_cp").style.display = "flex";
+                }
+            }
+    </script>
 
 </head>
 
@@ -44,7 +66,7 @@
     <main class="main">
         
         <div class="register_block">
-        <form action="php/registrar_comercio.php" method="post">
+        <form action="proveedor/php/registrar_comercio.php" method="post">
                 <div class="header">
                     <h1>Registrate como comercio</h1>
                 </div>
@@ -53,6 +75,29 @@
                         <div class="ubicacion">
                             <!-- direccion -->
                             <h3>Datos de ubicacion</h3>
+                            <!-- dejamos el mismo nombre por el css -->
+                            <div class="localidad"> 
+                                <label for="provin">Provincia</label>
+                                <input list="provin" name="provincia" id="provincia">
+                                <datalist id="provin">
+                                    <?php foreach($provincias as $valor): ?>
+                                    <option> <?= $valor['nombre'] ?></option>
+                                    <?php endforeach; ?>
+                                </datalist>
+                            </div>
+                            <div class="localidad">
+                                <label for="local">Localidad</label>
+                                <input list="local" name="localidad" id="localidad" onChange="codigoPostal();">
+                                <datalist id="local">
+                                    <?php foreach($localidades as $clave): ?>
+                                    <option value="<?= $clave['nombre'] ?>"> <?= $clave['codigo_postal'] ?></option>
+                                    <?php endforeach; ?>
+                                </datalist>
+                                <label for="cp" id="label_cp">C.P.</label>
+                                <input type="number" name="cp" id="cp" maxlength=4>
+                            </div>
+
+
                             <label for="text">Barrio:</label>
                             <input type="text" name="barrio" id="barrio" autocomplete=off maxlength="20">
                             <label for="text">Calle:</label>
--- proveedor/php/registrar_comercio.php
+++ proveedor/php/registrar_comercio.php
@@ -5,68 +5,107 @@
     $altura = isset($_POST['altura'])?$_POST['altura'] : null;
     $piso = isset($_POST['piso'])?$_POST['piso'] : null;
     $departamento = isset($_POST['dpto'])?$_POST['dpto'] : null;
-    
+    $provincia = isset($_POST['provincia'])?$_POST['provincia']:null;
+    $localidad = isset($_POST['localidad'])?$_POST['localidad']:null;
 
-     // inicia la sesion para poder dar uso de las variables
-     session_start();
-    try{
-        include_once ("conexion.php");
-
-        //Validacion de la licencia comercial (no puede repetirse)
-        $licencias_cargadas = $base->query(
-        "SELECT licencia_comercial 
-        FROM comercio 
-        WHERE licencia_comercial=$licencia")->fetchAll();
-
-        if(count($licencias_cargadas)==0){
-            //En este punto sabemos que no hay ninguna licencia
-            $base->beginTransaction();
-
-            //  cargamos al proveedor
-            $base->prepare("INSERT INTO poveedor(id_persona,Id_rubro,cuil/cuit,ventas_concretadas,suma_puntaje,status_proveedor,id_estado,productor_local)
-                VALUES(:id_persona,:Id_rubro,:cuil/cuit,:ventas_concretadas,:suma_puntaje,:status_proveedor,:id_estado,:productor_local)");
-            $base->execute(array(
-                ":id_persona"=>$_SESSION['id'],
-                ":Id_rubro"=>$_SESSION['rubro'],
-                ":cuil/cuit"=>$_SESSION['cuilt'],
-                // ventas concretadas hardcodeado a 0 al igual que suma puntaje
-                ":ventas_concretadas"=>0,
-                ":suma_puntaje"=>0,
-                // hardcodeado a 2 que indica status regular
-                ":status_proveedor"=>2,
-                //estado hardcodeado a 4 que es "En espera"
-                ":id_estado"=>4,
-                ":productor_local"=>$_SESSION['plocal']
-            ));
-
-            $id_proveedor = $base->lastInsertId("proveedor");
-            // cargamos la direccion del comercio *hacer las modificaciones en el formulario
-
-            // cargamos al comercio
-            $base->prepare("INSERT INTO comercio(id_proveedor,id_domicilio,id_rubro,id_categoria,nombre_comercio,delivery,licencia_comercial,pagina_web,mail,descripcion,telefono_comercial)
-                VALUES(:id_proveedor,:id_domicilio,:id_rubro,:id_categoria,:nombre_comercio,:delivery,:licencia_comercial,:pagina_web,:mail,:descripcion,:telefono_comercial)");
-            $base->execute(array(
-                ":id_proveedor",
-                ":id_domicilio",
-                ":id_rubro",
-                ":id_categoria",
-                ":nombre_comercio",
-                ":delivery",
-                ":licencia_comercial",
-                ":pagina_web",
-                ":mail",
-                ":descripcion",
-                ":telefono_comercial"
-            ));
-
-            $base->commit();
-        }else{
-            //Existe una licencia previamente cargada(volvemos con ese parametro lc=licencia cargada)
-            header("Location: ../form_comercio.php?lc=1");
-        }
-        
-    }catch(PDOException $ex){
-        $base->rollback();
-        echo($ex->getMessage());
+    // verificamos que la localidad este cargada o sea una nueva
+    include_once("conexion.php");
+    $localidad_cargada = $base->query("SELECT * FROM localidad WHERE nombre='$localidad'")->fetch(PDO::FETCH_ASSOC);
+    $cant_localidad = count($localidad_cargada);
+    if($cant_localidad>0){
+        echo("hola");
+        $id_localidad = $localidad_cargada["id_localidad"];
+    }else{
+        $codigo_postal = isset($_POST['cp'])?$_POST['cp'] : null;
     }
+    
+    session_start();
+    echo("nombre: ".$_SESSION['nombre']);
+    echo("<br>");
+    echo("licencia: ".$_SESSION['licencia']);
+    echo("<br>");
+    echo("categoria: ".$_SESSION['categ']);
+    echo("<br>");
+    echo("website: ".$_SESSION['website']);
+    echo("<br>");
+    echo("mail: ".$_SESSION['mail']);
+    echo("<br>");
+    echo("telefono: ".$_SESSION['telefono']);
+    echo("<br>");
+    echo("delivery: ".$_SESSION['delivery']);
+    echo("<br>");
+    echo("descripcion: ".$_SESSION['descripcion']);
+    echo("<br>");
+    echo("cuilt: ".$_SESSION['cuilt']);
+    echo("<br>");
+    echo("rubro: ".$_SESSION['rubro']);
+    echo("<br>");
+    echo("producto local: ".$_SESSION['plocal']);
+    echo("<br>");
+    echo("id usuario: ".$_SESSION['id']);
+    echo("<br>");
+    echo("barrio: ".$barrio);
+    echo("<br>");
+    echo("calle: ".$calle);
+    echo("<br>");
+    echo("altura: ".$altura);
+    echo("<br>");
+    echo("piso: ".$piso);
+    echo("<br>");
+    echo("departamento: ".$departamento);
+    echo("<br>");
+    echo("provincia: ".$provincia);
+    echo("<br>");
+    echo("localidad: ".$localidad);
+
+    // ya tenemos todos los dats que necesitamos
+    // try{
+    //     // la licencia ya fue validada igual que el cuit/cuil
+    //     //En este punto sabemos que sno hay ninguna licencia
+    //     $base->beginTransaction();
+
+    //     //  cargamos al proveedor
+    //     $base->prepare("INSERT INTO poveedor(id_persona,Id_rubro,cuil/cuit,ventas_concretadas,suma_puntaje,status_proveedor,id_estado,productor_local)
+    //         VALUES(:id_persona,:Id_rubro,:cuil/cuit,:ventas_concretadas,:suma_puntaje,:status_proveedor,:id_estado,:productor_local)");
+    //     $base->execute(array(
+    //         ":id_persona"=>$_SESSION['id'],
+    //         ":Id_rubro"=>$_SESSION['rubro'],
+    //         ":cuil/cuit"=>$_SESSION['cuilt'],
+    //         // ventas concretadas hardcodeado a 0 al igual que suma puntaje
+    //         ":ventas_concretadas"=>0,
+    //         ":suma_puntaje"=>0,
+    //         // hardcodeado a 2 que indica status regular
+    //         ":status_proveedor"=>2,
+    //         //estado hardcodeado a 4 que es "En espera"
+    //         ":id_estado"=>4,
+    //         ":productor_local"=>$_SESSION['plocal']
+    //     ));
+
+    //     $id_proveedor = $base->lastInsertId("proveedor");
+    //     // cargamos la direccion del comercio
+        
+
+    //     // cargamos al comercio
+    //     $base->prepare("INSERT INTO comercio(id_proveedor,id_domicilio,id_rubro,id_categoria,nombre_comercio,delivery,licencia_comercial,pagina_web,mail,descripcion,telefono_comercial)
+    //         VALUES(:id_proveedor,:id_domicilio,:id_rubro,:id_categoria,:nombre_comercio,:delivery,:licencia_comercial,:pagina_web,:mail,:descripcion,:telefono_comercial)");
+    //     $base->execute(array(
+    //         ":id_proveedor",
+    //         ":id_domicilio",
+    //         ":id_rubro",
+    //         ":id_categoria",
+    //         ":nombre_comercio",
+    //         ":delivery",
+    //         ":licencia_comercial",
+    //         ":pagina_web",
+    //         ":mail",
+    //         ":descripcion",
+    //         ":telefono_comercial"
+    //     ));
+
+    //     $base->commit();
+
+    // }catch(PDOException $ex){
+    //     $base->rollback();
+    //     echo($ex->getMessage());
+    // }
 ?>
