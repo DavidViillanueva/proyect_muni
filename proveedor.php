6236563b9b0e01833fbd82b406c420afb988e314
mohi22 <mohi_migue2210@yahoo.com.ar>
2020-10-20 23:03:15 -0300

    proveedores.php + PagProveedor.css

---
+++ proveedor.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+    
+    <link rel="icon" href="img/logo-small.png" type="text/png"/>
+    
+    <link rel="stylesheet" type="text/css" href="css/estilos_popup.css">
+    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    
+    <link rel="stylesheet" type="text/css" href="css/tabla-comercio.css">
+    
+    <link rel="stylesheet" type="text/css" href="css/PagProveedor.css">
+
+    <title>Proveedor</title>
+	
+</head>
+<body background="img/fondoazul2.png">
+	
+    <?php include_once('header.php'); ?>
+        <main>
+            <div class="catalogo">
+				<ul>
+					<li>
+						<img src="img/ubicacion.jpg" alt="">
+						<p>
+						   Búsqueda por cercanía, 
+						   encontrar de manera sencilla productos y/o servicios. 
+                            <<input name="" id="" class="btn btn-primary" type="button" value="agregar">
+						</p>
+					</li>
+					<li>
+						<img src="img/imagen1.jpg" alt="">
+						<p>
+						   Publicidad para potenciar tu negocio obteniendo mayor visibilidad comercial 
+						   TOTALMENTE GRATIS.
+						</p>
+					</li>
+					<li>
+						<img src="img/imagen2.png" alt="">
+						<p>
+							Esto te permitirá mejorar la relacion con tus viejos clientes y atraer a nuevos.
+						</p>
+                    </li>
+                    <li>
+						<img src="img/ubicacion.jpg" alt="">
+						<p>
+						   Búsqueda por cercanía, 
+						   encontrar de manera sencilla productos y/o servicios. 
+
+						</p>
+					</li>
+					<li>
+						<img src="img/imagen1.jpg" alt="">
+						<p>
+						   Publicidad para potenciar tu negocio obteniendo mayor visibilidad comercial 
+						   TOTALMENTE GRATIS.
+						</p>
+					</li>
+					<li>
+						<img src="img/imagen2.png" alt="">
+						<p>
+							Esto te permitirá mejorar la relacion con tus viejos clientes y atraer a nuevos.
+						</p>
+                    </li>
+                    <li>
+						<img src="img/imagen1.jpg" alt="">
+						<p>
+						   Publicidad para potenciar tu negocio obteniendo mayor visibilidad comercial 
+						   TOTALMENTE GRATIS.
+						</p>
+					</li>
+					<li>
+						<img src="img/imagen2.png" alt="">
+						<p>
+							Esto te permitirá mejorar la relacion con tus viejos clientes y atraer a nuevos.
+						</p>
+					</li>
+				</ul>
+            </div>
+        </main>
+    <?php include_once('footer.php'); ?>
+</body>
+    
+</html>
